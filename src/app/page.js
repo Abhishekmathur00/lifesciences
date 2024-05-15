895925458edd2426ead468cbd7c29edfd4c6f599
Abhishekmathur00 <Abhishekmathur7575@gmail.com>
2024-05-15 11:23:05 +0530

    try5

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,16 @@
 // import Banner from "@/components/homepage/Banner";
 // import Inquiryform from "@/components/homepage/Inquiryform";
 // import Last from "@/components/homepage/Last";
-import Management from "../components/homepage/Management";
-import Offerings from "@/components/homepage/Offerings";
-import WhychooseUs from "@/components/homepage/WhychooseUs";
-import Information from "@/components/homepage/Information";
-import Products from "@/components/homepage/Products";
-import Videosection from "@/components/homepage/Videosection";
+// import Management from "../components/homepage/Management";
+// import Offerings from "@/components/homepage/Offerings";
+// import WhychooseUs from "@/components/homepage/WhychooseUs";
+// import Information from "@/components/homepage/Information";
+// import Products from "@/components/homepage/Products";
+// import Videosection from "@/components/homepage/Videosection";
 export default function Home() {
   return (
-    <div>
-      <Banner />
+    <div>page
+      {/* <Banner />
       <WhychooseUs />
       <Videosection />
       <Offerings />
@@ -18,7 +18,7 @@ export default function Home() {
       <Products />
       <Inquiryform />
       <Management />
-      <Last />
+      <Last /> */}
     </div>
   );
 }
